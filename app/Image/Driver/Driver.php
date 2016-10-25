13f45a241a884a13d9321fbede341618c5f38142
Dan Bower <dan.bower88@gmail.com>
2016-10-25 20:39:43 +0100

    Add interface for creating App\Image\Image objects

---
+++ app/Image/Driver/Driver.php
@@ -0,0 +1,51 @@
+<?php namespace App\Image\Driver;
+
+use App\Image\Image;
+use App\Image\Colour;
+use InvalidArgumentException;
+
+/**
+ * A driver for creating images
+ */
+interface Driver
+{
+    /**
+     * Create the underlying image and create an Image instance with it.
+     *
+     * @param int $width
+     * @param int $height
+     * @param Colour $colour
+     *
+     * @return Image
+     */
+    public function canvas($width, $height, Colour $colour);
+
+    /**
+     * Write text onto the underlying image.
+     *
+     * @param Image $image
+     * @param string $text
+     * @param Colour $colour
+     */
+    public function writeText(Image $image, $text, Colour $colour);
+
+    /**
+     * Generate binary data in PNG format from the underlying image
+     * and call appropriate setters on the Image instance.
+     *
+     * @param Image $image
+     *
+     * @throws InvalidArgumentException unsupported on this system
+     */
+    public function encodePng(Image $image);
+
+    /**
+     * Generate binary data in GIF format from the underlying image
+     * and call appropriate setters on the Image instance.
+     *
+     * @param Image $image
+     *
+     * @throws InvalidArgumentException unsupported on this system
+     */
+    public function encodeGif(Image $image);
+}
