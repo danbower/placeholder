efb642fbcb7096107679421a5883124a4b633309
Dan Bower <dan.bower88@gmail.com>
2016-10-26 15:06:52 +0100

    Add class for producing images based on a config

---
+++ app/Image/Drawer.php
@@ -0,0 +1,59 @@
+<?php namespace App\Image;
+
+use App\Image\Config\Config;
+use App\Image\Driver\Driver;
+use InvalidArgumentException;
+
+/**
+ * Produces an image based on some configuration.
+ */
+class Drawer
+{
+    /**
+     * @var Driver $driver
+     */
+    protected $driver;
+
+    /**
+     * @param Driver $driver
+     */
+    public function __construct(Driver $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    /**
+     * @param Config $config
+     *
+     * @return Image
+     */
+    public function draw(Config $config)
+    {
+        $image = $this->driver->canvas(
+            $config->getWidth(),
+            $config->getHeight(),
+            $config->getBackgroundColour()
+        );
+
+        $this->driver->writeText(
+            $image,
+            $config->getText(),
+            $config->getForegroundColour()
+        );
+
+        switch ($config->getFormat()) {
+            case 'png':
+                $this->driver->encodePng($image);
+                break;
+
+            case 'gif':
+                $this->driver->encodeGif($image);
+                break;
+
+            default:
+                throw new InvalidArgumentException('Passed invalid config');
+        }
+
+        return $image;
+    }
+}
---
+++ tests/Unit/Image/DrawerTest.php
@@ -0,0 +1,136 @@
+<?php namespace Tests\Image;
+
+use App\Image\Image;
+use App\Image\Drawer;
+use App\Image\Colour;
+use App\Image\Config\Config;
+use App\Image\Driver\Driver;
+use InvalidArgumentException;
+use PHPUnit_Framework_TestCase;
+
+class DrawerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Create a Config mock instance.
+     */
+    protected function createConfigMock()
+    {
+        $config = $this->createMock(Config::class);
+        $config->expects($this->once())
+               ->method('getWidth');
+
+        $config->expects($this->once())
+               ->method('getHeight');
+
+        $config->expects($this->once())
+               ->method('getText');
+
+        $config->expects($this->once())
+               ->method('getBackgroundColour')
+               ->will($this->returnValue(
+                   $this->createMock(Colour::class)
+               ));
+
+        $config->expects($this->once())
+               ->method('getForegroundColour')
+               ->will($this->returnValue(
+                   $this->createMock(Colour::class)
+               ));
+
+        return $config;
+    }
+
+    /**
+     * Create a Driver mock instance.
+     */
+    protected function createDriverMock()
+    {
+        $driver = $this->createMock(Driver::class);
+        $driver->expects($this->once())
+               ->method('canvas')
+               ->will($this->returnValue(
+                   $this->createMock(Image::class)
+               ));
+
+        $driver->expects($this->once())
+               ->method('writeText');
+
+        return $driver;
+    }
+
+    /**
+     * Tests that PNG encoding is handled.
+     */
+    public function testEncodePng()
+    {
+        $config = $this->createConfigMock();
+        $config->expects($this->once())
+               ->method('getFormat')
+               ->will($this->returnValue('png'));
+
+        $driver = $this->createDriverMock();
+        $driver->expects($this->once())
+               ->method('encodePng');
+
+        $drawer = new Drawer($driver);
+        $image = $drawer->draw($config);
+
+        $this->assertInstanceOf(Image::class, $image);
+    }
+
+    /**
+     * Tests that GIF encoding is handled.
+     */
+    public function testEncodeGif()
+    {
+        $config = $this->createConfigMock();
+        $config->expects($this->once())
+               ->method('getFormat')
+               ->will($this->returnValue('gif'));
+
+        $driver = $this->createDriverMock();
+        $driver->expects($this->once())
+               ->method('encodeGif');
+
+        $drawer = new Drawer($driver);
+        $image = $drawer->draw($config);
+
+        $this->assertInstanceOf(Image::class, $image);
+    }
+
+    /**
+     * Tests that attempting to draw with an unsupported format
+     * produces an exception.
+     */
+    public function testInvalidFormat()
+    {
+        $config = $this->createMock(Config::class);
+        $config->expects($this->once())
+               ->method('getFormat')
+               ->will($this->returnValue('foo'));
+
+        $config->expects($this->once())
+               ->method('getBackgroundColour')
+               ->will($this->returnValue(
+                   $this->createMock(Colour::class)
+               ));
+
+        $config->expects($this->once())
+               ->method('getForegroundColour')
+               ->will($this->returnValue(
+                   $this->createMock(Colour::class)
+               ));
+
+        $driver = $this->createMock(Driver::class);
+        $driver->expects($this->once())
+               ->method('canvas')
+               ->will($this->returnValue(
+                   $this->createMock(Image::class)
+               ));
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $drawer = new Drawer($driver);
+        $drawer->draw($config);
+    }
+}
